dfdf89594766eefedff6c2c48f9d68129097d856
Mateusz Karandys <mateusz.karandys@gmail.com>
2022-09-26 20:07:25 +0200

    Add movement

--- asteroid.py
+++ asteroid.py
@@ -18,3 +18,25 @@ class Asteroid(Circle):
 
         Circle.__init__(self, color, x, y, radius)
         self.mass = mass
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.acceleration_x = 0
+        self.acceleration_y = 0
+        self.force_x = 0
+        self.force_y = 0
+        self.collide = False
+
+    def set_acceleration(self, acc_x, acc_y):
+        self.acceleration_x = acc_x
+        self.acceleration_y = acc_y
+
+    def update(self):
+
+        if self.collide:
+            return
+
+        self.velocity_x += self.acceleration_x
+        self.velocity_y += self.acceleration_y
+
+        self.x += self.velocity_x
+        self.y += self.velocity_y
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import math
 import pygame
 
 from planet import Planet
@@ -13,11 +14,49 @@ BACKGROUND_COLOR = (121, 121, 200)
 
 # Sample
 PLANETS = [
-    Planet((67, 26, 9), 45, 200, 60, 200),
-    Planet((90, 80, 137), 400, 80, 80, 300)
+    Planet((67, 26, 9), 800, 0, 60, 300),
+    Planet((67, 26, 9), 0, 400, 60, 200),
+    # Planet((90, 80, 137), 400, 80, 80, 1000)
 ]
+CONSTANT_G = 100
 
-def draw_window(asteroid):
+
+
+def handle_forces(asteroid: Asteroid):
+    acceleration_x = 0
+    acceleration_y = 0
+
+    for planet in PLANETS:
+        dist = math.sqrt((asteroid.x - planet.x)**2 + (asteroid.y - planet.y)**2)
+        sin = (planet.y - asteroid.y) / dist
+        cos = (planet.x - asteroid.x) / dist
+        
+        acceleration_x += CONSTANT_G * planet.mass * cos / (dist ** 2)
+        acceleration_y += CONSTANT_G * planet.mass * sin / (dist ** 2)
+
+    asteroid.set_acceleration(acceleration_x, acceleration_y)
+
+def handle_collisions(asteroid: Asteroid):
+
+    if asteroid.x <= 0:
+        asteroid.x = 0
+        asteroid.collide = True
+    if asteroid.x >= WIDTH:
+        asteroid.x = WIDTH
+        asteroid.collide = True
+    if asteroid.y <= 0:
+        asteroid.y = 0
+        asteroid.collide = True
+    if asteroid.y >= HEIGHT:
+        asteroid.y = HEIGHT
+        asteroid.collide = True
+
+    for planet in PLANETS:
+        dist = math.sqrt((asteroid.x - planet.x)**2 + (asteroid.y - planet.y)**2)
+        if asteroid.radius + planet.radius >= dist:
+            asteroid.collide = True
+
+def draw_window(asteroid: Asteroid):
     WINDOW.fill(BACKGROUND_COLOR)
 
     for planet in PLANETS:
@@ -27,12 +66,19 @@ def draw_window(asteroid):
 
     pygame.display.update()
 
+def update(asteroid: Asteroid):
+    
+    handle_collisions(asteroid)
+    handle_forces(asteroid)
+    
+    asteroid.update()
+
 def main():
 
     pygame.init()
     pygame.display.set_caption(CAPTION)
 
-    asteroid = Asteroid()
+    asteroid = Asteroid(x=400, y=375)
 
     running = True
     clock = pygame.time.Clock()
@@ -45,6 +91,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+        update(asteroid)
         draw_window(asteroid)
 
     pygame.quit()
